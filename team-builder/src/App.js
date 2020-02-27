1e35fd65b854af6d4e7791ef3fb5b46b77f28e76
Kenji Greene <kenjigrr@gmail.com>
2020-02-27 02:09:26 -0500

    finished mvp

---
+++ team-builder/src/App.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import "./App.css";
+import Form from './components/Form';
+import Members from './components/Members';
+
+function App() {
+  const [teamList, setTeamList] = useState([]);
+  function addNewMember (prop) {
+    const member = {
+      id: Date.now(),
+      name: prop["name"],
+      email: prop["email"],
+      role: prop["role"],
+    };
+    setTeamList([...teamList, member]);
+  }
+  function deleteMember (id) {
+    const memberFilter = teamList.filter(member =>{
+      return member.id !== id;
+    });
+    setTeamList(memberFilter);
+  }
+  return (
+    <div className="App">
+      <Form team={addNewMember} />
+      <Members memberList={teamList} deleteMember={deleteMember} />
+    </div>
+  );
+}
+export default App;
